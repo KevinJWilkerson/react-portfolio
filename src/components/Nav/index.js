fb9ab3a6082315d7a3d592ebc17bd2335c582de2
Kevin <kevin.j.wilkerson@gmail.com>
2022-04-19 09:20:09 -0500

    added hover styling to nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,7 +13,9 @@ function Nav(props) {
       <ul className="flex-row">
         {pages.map((Page) => (
           <li
-            className={`mx-2 ${currentPage.name === Page.name && "navActive"}`}
+            className={`mx-2 on-hover ${
+              currentPage.name === Page.name && "navActive"
+            }`}
             key={Page.name}
           >
             <span onClick={() => setCurrentPage(Page)}>
